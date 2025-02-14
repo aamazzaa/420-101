b82a99510e0dcec21e0c7439b1cbd826cbb747db
Anthony Mazza <anthonyrobertmazza@gmail.com>
2025-02-14 12:38:19 -0500

    Lab 4 part 3

--- MusicOrganizer.java
+++ MusicOrganizer.java
@@ -1,49 +1,184 @@
 import java.util.ArrayList;
-public class MusicOrganizer {
-    private ArrayList<String> files;
-    private MusicPlayer player;
 
-    public MusicOrganizer() {
+/**
+ * A class to hold details of audio files.
+ * 
+ * @author David J. Barnes and Michael Kölling
+ * @version 2016.02.29
+ */
+public class MusicOrganizer
+{
+    // An ArrayList for storing the file names of music files.
+    private ArrayList<String> files;
+    // A player for the music files.
+    private MusicPlayer player;
+        
+    /**
+     * Create a MusicOrganizer
+     */
+    public MusicOrganizer()
+    {
         files = new ArrayList<>();
         player = new MusicPlayer();
     }
-
-    public void addFile(String filename) {
+    
+    /**
+     * Add a file to the collection.
+     * @param filename The file to be added.
+     */
+    public void addFile(String filename)
+    {
         files.add(filename);
     }
-
-    public int getNumberOfFiles() {
+    
+    /**
+     * Return the number of files in the collection.
+     * @return The number of files in the collection.
+     */
+    public int getNumberOfFiles()
+    {
         return files.size();
     }
-
-    public void listFile(int index) {
-        if (index >= 0 && index < files.size()) {
+    
+    /**
+     * List a file from the collection.
+     * @param index The index of the file to be listed.
+     */
+    public void listFile(int index)
+    {
+        if(validIndex(index)) {
             String filename = files.get(index);
             System.out.println(filename);
         }
     }
-
-    public void removeFile(int index) {
-        if (index >= 0 && index < files.size()) {
+    
+    /**
+     * Show a list of all the files in the collection.
+     */
+    public void listAllFiles()
+    {
+        int position = 0; // Local variable to track index
+        for(String filename : files) {
+            System.out.println(position + ": " + filename);
+            position++; // Increment index
+        }
+    }
+    
+    /**
+     * Remove a file from the collection.
+     * @param index The index of the file to be removed.
+     */
+    public void removeFile(int index)
+    {
+        if(validIndex(index)) {
             files.remove(index);
         }
     }
 
-    public void startPlaying(int index) {
-        String filename = files.get(index);
-        player.startPlaying(filename);
+    /**
+     * Start playing a file in the collection.
+     * Use stopPlaying() to stop it playing.
+     * @param index The index of the file to be played.
+     */
+    public void startPlaying(int index)
+    {
+        if(validIndex(index)) {
+            String filename = files.get(index);
+            player.startPlaying(filename);
+        }
     }
 
-    public void stopPlaying() {
+    /**
+     * Stop the player.
+     */
+    public void stopPlaying()
+    {
         player.stop();
     }
 
     /**
-     * List all the files in the collection.
+     * Play a file in the collection. Only return once playing has finished.
+     * @param index The index of the file to be played.
      */
-    public void listAllFiles() {
-        for (String file : files) {
-            System.out.println(file);
+    public void playAndWait(int index)
+    {
+        if(validIndex(index)) {
+            String filename = files.get(index);
+            player.playSample(filename);
         }
+    }
+
+    /**
+     * Determine whether the given index is valid for the collection.
+     * Print an error message if it is not.
+     * @param index The index to be checked.
+     * @return true if the index is valid, false otherwise.
+     */
+    private boolean validIndex(int index)
+    {
+        // The return value.
+        // Set according to whether the index is valid or not.
+        boolean valid;
+        
+        if(index < 0) {
+            System.out.println("Index cannot be negative: " + index);
+            valid = false;
+        }
+        else if(index >= files.size()) {
+            System.out.println("Index is too large: " + index);
+            valid = false;
+        }
+        else {
+            valid = true;
+        }
+        return valid;
+    }
+
+    /**
+     * List all files that match the given search string.
+     * @param searchString The string to match.
+     */
+    public void listMatching(String searchString) {
+        boolean found = false;
+        for (String filename : files) {
+            if (filename.contains(searchString)) {
+                System.out.println(filename);
+                found = true;
+            }
+        }
+        if (!found) {
+            System.out.println("No files match the search string: " + searchString);
+        }
+    }
+
+    /**
+     * Play samples of all tracks by a particular artist.
+     * @param artist The artist's name.
+     */
+    public void playSamplesByArtist(String artist) {
+        for (String filename : files) {
+            if (filename.contains(artist)) {
+                player.playSample(filename);
+            }
+        }
+    }
+
+    /**
+     * Check if a number is prime.
+     * @param n The number to check.
+     * @return true if the number is prime, false otherwise.
+     */
+    public boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        int divisor = 2;
+        while (divisor < n) {
+            if (n % divisor == 0) {
+                return false;
+            }
+            divisor++;
+        }
+        return true;
     }
 }
